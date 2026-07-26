4f6feb284a5f5649e9a4fd52da56f559a5e1baf8
SubhraMishra <msubhra532@gmail.com>
2026-07-27 00:20:40 +0530

    Initial commit

---
+++ ProductCatalogue/Product-catalogue/src/CategoryFilter.jsx
@@ -0,0 +1,14 @@
+const CategoryFilter=({ categories,onSelect }) =>{
+    console.log(categories);
+    return (
+      <select id="categorySelect" className="form-control" onChange={(e)=>onSelect(e.target.value)}>
+        <option value="">All Categories</option>
+        {categories.map(category=>(
+               <option key={category.id} value={category.id}>{category.name}</option>
+            ))}
+      </select>
+        
+    )
+}
+
+export default CategoryFilter;
